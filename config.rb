8a4f9ad38ed3274e9a926dde57a022eda78462c4
Shan Ricciardi <shan@ricciardidesigns.com>
2013-02-15 20:27:44 -0800

    first draft of base site

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+require "/Library/Ruby/Gems/1.8/gems/susy-1.0/lib/susy.rb"
+require "/Users/shan/.rvm/gems/ruby-1.9.3-p327/gems/sassy-buttons-0.1.4/lib/sassy-buttons.rb"
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
